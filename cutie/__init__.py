791f4c0ff803e8635b004c4a9c39690d88f44e99
Kevin Bu <kbpi314@gmail.com>
2018-08-16 12:49:53 -0400

    added init and output

---
+++ cutie/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+__author__ = "The Clemente Lab"
+__copyright__ = "Copyright 2017, The Clemente Lab"
+__credits__ = ["Jose C. Clemente", "Kevin Bu"]
+__license__ = "GPL"
+__version__ = "0.1.0-dev"
+__maintainer__ = "Kevin Bu"
+__email__ = "kbu314@gmail.com"
+
+__all__ = ['parse','statistics','output']
