1afed2d5f9f123203b90d17514c8bc783e257425
Kevin Bu <kbpi314@gmail.com>
2019-04-16 12:24:29 -0400

    added initial stats MINE

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -41,11 +41,110 @@ class TestStatistics(unittest.TestCase):
                                    [1,2,3],
                                    [3,6,2]])
 
+        self.true_paired_sampvar1_arrays = {
+            'stats.pearsonr': (np.array([
+                [1.        , 0.95279653, 0.93951252],
+                [0.95279653, 1.        , 0.99696214],
+                [0.93951252, 0.99696214, 1.        ]]), np.array([
+                [0.        , 0.0122235 , 0.01769506],
+                [0.0122235 , 0.        , 0.0002009 ],
+                [0.01769506, 0.0002009 , 0.        ]])),
+            'stats.spearmanr': (np.array([
+                [1.        , 0.15389675, 0.15789474],
+                [0.15389675, 1.        , 0.97467943],
+                [0.15789474, 0.97467943, 1.        ]]), np.array([
+                [0.00000000e+00, 8.04828817e-01, 7.99800666e-01],
+                [8.04828817e-01, 1.40426542e-24, 4.81823047e-03],
+                [7.99800666e-01, 4.81823047e-03, 0.00000000e+00]])),
+            'stats.kendalltau': (np.array([
+                [1.        , 0.10540926, 0.11111111],
+                [0.10540926, 1.        , 0.9486833 ],
+                [0.11111111, 0.9486833 , 1.        ]]), np.array([
+                [0.01917673, 0.80054211, 0.79468572],
+                [0.80054211, 0.01430588, 0.0229774 ],
+                [0.79468572, 0.0229774 , 0.01917673]])),
+            'minepy': (np.array([
+                [1.        , 0.41997309, 0.41997309],
+                [0.41997309, 1.        , 0.97095059],
+                [0.41997309, 0.97095059, 1.        ]]), np.array([
+                [2.5600000e-07, 2.9577243e-02, 2.9577243e-02],
+                [2.9577243e-02, 2.5600000e-07, 2.5600000e-07],
+                [2.9577243e-02, 2.5600000e-07, 2.5600000e-07]]))}
+
+        self.pearson_stats = ['kpc', 'jkp' , 'bsp', 'rpc', 'rjkp', 'rbsp']
+        self.spearman_stats = ['ksc', 'jks','bss', 'rsc', 'rjks', 'rbss']
+        self.kendall_stats = ['kkc', 'jkk', 'bsk', 'rkc', 'rjkk', 'rbsk']
+        self.mine_stats = ['mine', 'jkm', 'bsm', 'rmine', 'rjkm', 'rbsm']
+
         self.functions = ['stats.pearsonr', 'stats.spearmanr', 'stats.kendalltau']
         self.mapf = {'stats.pearsonr': stats.pearsonr,
                      'stats.spearmanr': stats.spearmanr,
                      'stats.kendalltau': stats.kendalltau}
+
         self.minep_fp = 'n=50,alpha=0.6.csv'
+        # technically improper usage of the p value file but suffices
+        # for testing purposes (sample size does not match)
+        with open(os.path.dirname(os.path.realpath(__file__)) + '/' + \
+            self.minep_fp, 'r') as f:
+                self.mine_bins, self.pvalue_bins = parse.parse_minep(f, ',', 13)
+
+        self.assign_statistics_truths = {
+            'kpc': {
+            'pvalues': np.array([[0.        , 0.0122235 , 0.01769506],
+                                 [0.0122235 , 0.        , 0.0002009 ],
+                                 [0.01769506, 0.0002009 , 0.        ]]),
+            'logpvals': np.array([[    -np.inf, -4.40439492, -4.03446983],
+                                  [-4.40439492,     -np.inf, -8.51268655],
+                                  [-4.03446983, -8.51268655,     -np.inf]]),
+            'correlations': np.array([[1.        , 0.95279653, 0.93951252],
+                                      [0.95279653, 1.        , 0.99696214],
+                                      [0.93951252, 0.99696214, 1.        ]]),
+            'r2vals': np.array([[1.        , 0.90782123, 0.88268377],
+                                [0.90782123, 1.        , 0.99393351],
+                                [0.88268377, 0.99393351, 1.        ]])},
+
+            'ksc': {
+            'pvalues': np.array([[0.00000000e+00, 8.04828817e-01, 7.99800666e-01],
+                                 [8.04828817e-01, 1.40426542e-24, 4.81823047e-03],
+                                 [7.99800666e-01, 4.81823047e-03, 0.00000000e+00]]),
+            'logpvals': np.array([[        -np.inf,  -0.21712567,  -0.22339275],
+                                  [ -0.21712567, -54.9225279 ,  -5.33534854],
+                                  [ -0.22339275,  -5.33534854,         -np.inf]]),
+            'correlations': np.array([[1.        , 0.15389675, 0.15789474],
+                                      [0.15389675, 1.        , 0.97467943],
+                                      [0.15789474, 0.97467943, 1.        ]]),
+            'r2vals': np.array([[1.        , 0.02368421, 0.02493075],
+                                [0.02368421, 1.        , 0.95      ],
+                                [0.02493075, 0.95      , 1.        ]])},
+
+            'kkc': {
+            'pvalues': np.array([[0.01917673, 0.80054211, 0.79468572],
+                                 [0.80054211, 0.01430588, 0.0229774 ],
+                                 [0.79468572, 0.0229774 , 0.01917673]]),
+            'logpvals': np.array([[-3.95405776, -0.22246615, -0.22980857],
+                                  [-0.22246615, -4.24708475, -3.77324409],
+                                  [-0.22980857, -3.77324409, -3.95405776]]),
+            'correlations': np.array([[1.        , 0.10540926, 0.11111111],
+                                      [0.10540926, 1.        , 0.9486833 ],
+                                      [0.11111111, 0.9486833 , 1.        ]]),
+            'r2vals': np.array([[1.        , 0.01111111, 0.01234568],
+                                [0.01111111, 1.        , 0.9       ],
+                                [0.01234568, 0.9       , 1.        ]])},
+
+            'mine': {
+            'pvalues': np.array([[2.5600000e-07, 2.9577243e-02, 2.9577243e-02],
+                                 [2.9577243e-02, 2.5600000e-07, 2.5600000e-07],
+                                 [2.9577243e-02, 2.5600000e-07, 2.5600000e-07]]),
+            'logpvals': np.array([[-15.17808839,  -3.52075003,  -3.52075003],
+                                   [ -3.52075003, -15.17808839, -15.17808839],
+                                   [ -3.52075003, -15.17808839, -15.17808839]]),
+            'correlations': np.array([[1.        , 0.41997309, 0.41997309],
+                                      [0.41997309, 1.        , 0.97095059],
+                                      [0.41997309, 0.97095059, 1.        ]]),
+            'r2vals': np.array([[1.        , 0.1763774 , 0.1763774 ],
+                                [0.1763774 , 1.        , 0.94274506],
+                                [0.1763774 , 0.94274506, 1.        ]])}}
+
 
 
     def test_compute_pc(self):
@@ -74,51 +173,33 @@ class TestStatistics(unittest.TestCase):
 
 
     def test_initial_stats_SLR(self):
-        true_arrays = {
-            'stats.pearsonr': (np.array([
-                [1.        , 0.95279653, 0.93951252],
-                [0.95279653, 1.        , 0.99696214],
-                [0.93951252, 0.99696214, 1.        ]]), np.array([
-                [0.        , 0.0122235 , 0.01769506],
-                [0.0122235 , 0.        , 0.0002009 ],
-                [0.01769506, 0.0002009 , 0.        ]])),
-            'stats.spearmanr': (np.array([
-                [1.        , 0.15389675, 0.15789474],
-                [0.15389675, 1.        , 0.97467943],
-                [0.15789474, 0.97467943, 1.        ]]), np.array([
-                [0.00000000e+00, 8.04828817e-01, 7.99800666e-01],
-                [8.04828817e-01, 1.40426542e-24, 4.81823047e-03],
-                [7.99800666e-01, 4.81823047e-03, 0.00000000e+00]])),
-            'stats.kendalltau': (np.array([
-                [1.        , 0.10540926, 0.11111111],
-                [0.10540926, 1.        , 0.9486833 ],
-                [0.11111111, 0.9486833 , 1.        ]]), np.array([
-                [0.01917673, 0.80054211, 0.79468572],
-                [0.80054211, 0.01430588, 0.0229774 ],
-                [0.79468572, 0.0229774 , 0.01917673]]))}
-
         for stat in self.functions:
-            assert_almost_equal(true_arrays[stat],
+            assert_almost_equal(self.true_paired_sampvar1_arrays[stat],
                 statistics.initial_stats_SLR(self.samp_var1, self.samp_var1,
                     self.mapf[stat]), decimal=7)
 
     def test_initial_stats_MINE(self):
-
-        # technically improper usage of the p value file but suffices
-        # for testing purposes (sample size does not match)
-        with open(os.path.dirname(os.path.realpath(__file__)) + '/' + \
-            self.minep_fp, 'r') as f:
-                mine_bins, pvalue_bins = parse.parse_minep(f, ',', 13)
-
-        assert_almost_equal((np.array([
-            [1.        , 0.41997309, 0.41997309],
-            [0.41997309, 1.        , 0.97095059],
-            [0.41997309, 0.97095059, 1.        ]]), np.array([
-            [2.5600000e-07, 2.9577243e-02, 2.9577243e-02],
-            [2.9577243e-02, 2.5600000e-07, 2.5600000e-07],
-            [2.9577243e-02, 2.5600000e-07, 2.5600000e-07]])),
+        assert_almost_equal(self.true_paired_sampvar1_arrays['minepy'],
             statistics.initial_stats_MINE(np.shape(self.samp_var1)[1],
-                self.samp_var1, mine_bins, pvalue_bins), decimal=7)
+                self.samp_var1, self.mine_bins, self.pvalue_bins), decimal=7)
+
+    def test_assign_statistics(self):
+        # tests kpc, ksc, kkc and mine
+        for stat in ['kpc', 'ksc', 'kkc', 'mine']:
+            stat_to_matrix = statistics.assign_statistics(self.samp_var1,
+                self.samp_var1, stat, self.pearson_stats, self.spearman_stats,
+                self.kendall_stats, self.mine_stats, self.mine_bins,
+                self.pvalue_bins)
+
+            for k in stat_to_matrix.keys():
+                assert_almost_equal(self.assign_statistics_truths[stat][k],
+                                    stat_to_matrix[k], decimal=7)
+
+        # assertRaise for valuerror, 'kpp' not a valid stat string
+        self.assertRaises(ValueError, statistics.assign_statistics,
+            self.samp_var1, self.samp_var2, 'kpp',
+            self.pearson_stats, self.spearman_stats, self.kendall_stats,
+            self.mine_stats, self.mine_bins, self.pvalue_bins)
 
 
     def test_zero_replacement(self):
@@ -153,7 +234,8 @@ class TestStatistics(unittest.TestCase):
         self.assertRaises(ValueError, statistics.multi_zeros, samp_var)
 
     def test_log_transform(self):
-        tmpdir = '/Users/KevinBu/Documents/GitHub/CUtIe/tests/' # /tests/data_processing/
+         # /tests/data_processing/
+        tmpdir = os.path.dirname(os.path.realpath(__file__)) + '/'
         var_number = 2
 
         # Check no zeros are returned
