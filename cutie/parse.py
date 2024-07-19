e375d74cd67d356fd540a8804b00a0e1d8f2f4df
Kevin Bu <kbpi314@gmail.com>
2024-07-19 17:38:52 -0400

    testing metadata

--- cutie/parse.py
+++ cutie/parse.py
@@ -55,7 +55,7 @@ def parse_input(ftype, fp, startcol, endcol, delimiter, skip, metadata):
     # extract metadata
     print(metadata)
     print(type(metadata))
-    if metadata != ['None'] and metadata != "['None']":
+    if metadata != 'None' and metadata != ['None'] and metadata != "['None']":
         df_meta = df[metadata]
         df = df.drop(metadata, axis=1)
     else:
