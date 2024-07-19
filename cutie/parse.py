7270b98a5525e1522ac29a1e05f2005f6f11e1ae
Kevin Bu <kbpi314@gmail.com>
2024-07-19 17:43:43 -0400

    testing metadata

--- cutie/parse.py
+++ cutie/parse.py
@@ -53,8 +53,6 @@ def parse_input(ftype, fp, startcol, endcol, delimiter, skip, metadata):
         raise ValueError('Both startcol and endcol must be specified')
     
     # extract metadata
-    print(metadata)
-    print(type(metadata))
     if metadata != 'None' and metadata != ['None'] and metadata != "['None']":
         df_meta = df[metadata]
         df = df.drop(metadata, axis=1)
